010afd33dc44536a264272f06d5bfb584a88fffe
Prince <thedrkprns@gmail.com>
2021-04-03 23:43:54 +0530

    rollback

--- src/firebase/fbConfig.js
+++ src/firebase/fbConfig.js
@@ -3,11 +3,9 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 
-const APIKEY = process.env.REACT_APP_APIKEY
-
 const firebaseConfig = 
 {
-    apiKey: APIKEY,
+    apiKey: 'AIzaSyBh86q0PoUZCgTN9q8lBH0bZ-LtlYawyfU',
     authDomain: "kanban-42358.firebaseapp.com",
     projectId: "kanban-42358",
     storageBucket: "kanban-42358.appspot.com",
